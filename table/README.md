f9dfcaeff2d61a7fea15e4f729ee295e840d84ec
Chris Co <coswarm@gmail.com>
2022-06-23 20:30:29 +0300

    table component initial commit

---
+++ table/README.md
@@ -0,0 +1,1 @@
+https://webframe.app/docs
---
+++ table/index.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react'
+
+// Default export for React
+export default class Table extends PureComponent {
+  render () {
+    return (
+      <table className="table"></table>
+    )
+  }
+}
